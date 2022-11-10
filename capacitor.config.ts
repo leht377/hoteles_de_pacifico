46768548640faa2480dab9fd746337ee475f0ae3
leht377 <migithub2019@gmail.com>
2022-11-10 16:23:04 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hoteles_de_pacifico',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
