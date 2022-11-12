9506fb93e2083adb5231e214a77a23dfe95f3bd9
leht377 <migithub2019@gmail.com>
2022-11-12 10:02:30 -0500

    carpte folder eliminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'pages/lista-hoteles',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
-    path: 'lista-hoteles',
+    path: 'pages/lista-hoteles',
     loadChildren: () => import('./pages/lista-hoteles/lista-hoteles.module').then( m => m.ListaHotelesPageModule)
-  }
+  },
+
 ];
 
 @NgModule({
