3ee6bca5a279e2957af8d3c90ea4f6be296575c9
DAEespi <bhgespinel@gmail.com>
2022-12-18 20:03:51 -0500

    validacion del demonio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule, ReactiveFormsModule ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/pages/registrocliente/registrocliente.module.ts
+++ src/app/pages/registrocliente/registrocliente.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { RegistroclientePageRoutingModule } from './registrocliente-routing.module';
-
 import { RegistroclientePage } from './registrocliente.page';
-
+import { Validators, FormBuilder, FormGroup, FormControl,  } from '@angular/forms';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    RegistroclientePageRoutingModule
+    RegistroclientePageRoutingModule,
+    FormBuilder, 
+    FormGroup, 
+    FormControl,
+    
+   
   ],
   declarations: [RegistroclientePage]
 })
